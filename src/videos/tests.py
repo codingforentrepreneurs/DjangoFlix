6beee7e5e38001690c16c7abc74559558435287e
CFE <hello@teamcfe.com>
2021-03-14 13:33:58 -0700

    9 - Your First TestCase

--- src/videos/tests.py
+++ src/videos/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Video
+
+class VideoModelTestCase(TestCase):
+    def setUp(self):
+        Video.objects.create(title='This is my title')
+    
+    def test_valid_title(self):
+        title='This is my title'
+        qs = Video.objects.filter(title=title)
+        self.assertTrue(qs.exists())
+
+    def test_created_count(self):
+        qs = Video.objects.all()
+        self.assertEqual(qs.count(), 1)
