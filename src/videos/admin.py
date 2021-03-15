5942ba3a386ddc879a3d9980213668db057ff871
CFE <hello@teamcfe.com>
2021-03-15 13:06:59 -0700

    14 - CharField Choices for Publishing Videos

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 from .models import VideoAllProxy, VideoPublishedProxy
 
 class VideoAllAdmin(admin.ModelAdmin):
-    list_display = ['title', 'id', 'video_id', 'is_published']
+    list_display = ['title', 'id', 'state', 'video_id', 'is_published']
     search_fields = ['title']
-    list_filter = ['active']
-    readonly_fields = ['id', 'is_published']
+    list_filter = ['state', 'active']
+    readonly_fields = ['id', 'is_published', 'publish_timestamp']
     class Meta:
         model = VideoAllProxy
 
--- src/videos/models.py
+++ src/videos/models.py
@@ -1,7 +1,14 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class Video(models.Model):
+    class VideoStateOptions(models.TextChoices):
+        # CONSTANT = DB_VALUE, USER_DISPLAY_VA
+        PUBLISH = 'PU', 'Publish'
+        DRAFT = 'DR', 'Draft'
+        # UNLISTED = 'UN', 'Unlisted'
+        # Private = 'PR', 'Private'
+
     title = models.CharField(max_length=220)
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True) # 'this-is-my-video'
@@ -9,12 +16,20 @@ class Video(models.Model):
     active = models.BooleanField(default=True)
     # timestamp
     # updated 
-    # state 
-    # publish_timestamp
+    state = models.CharField(max_length=2, choices=VideoStateOptions.choices, default=VideoStateOptions.DRAFT)
+    publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
     
     @property
     def is_published(self):
         return self.active
+
+    def save(self, *args, **kwargs):
+        if self.state == self.VideoStateOptions.PUBLISH and self.publish_timestamp is None:
+            print("save as timestamp for published")
+            self.publish_timestamp = timezone.now()
+        elif self.state == self.VideoStateOptions.DRAFT:
+            self.publish_timestamp = None
+        super().save(*args, **kwargs)
 
 class VideoAllProxy(Video):
     class Meta:
