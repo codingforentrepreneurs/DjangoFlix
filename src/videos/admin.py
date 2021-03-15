e1fb3e2e50e9728caaec1c853cb7d27c86176ea8
CFE <hello@teamcfe.com>
2021-03-15 12:26:53 -0700

    12 - Django Admin Model Proxy

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Video, VideoProxy
+from .models import VideoAllProxy, VideoPublishedProxy
 
-admin.site.register(Video)
+class VideoAllAdmin(admin.ModelAdmin):
+    list_display = ['title', 'video_id']
+    search_fields = ['title']
+    # list_filter = ['video_id']
+    class Meta:
+        model = VideoAllProxy
 
 
-admin.site.register(VideoProxy)
+admin.site.register(VideoAllProxy, VideoAllAdmin)
+
+
+class VideoPublishedProxyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'video_id']
+    search_fields = ['title']
+    class Meta:
+        model = VideoPublishedProxy
+    
+    def get_queryset(self, request):
+        return VideoProxy.objects.filter(active=True)
+
+
+admin.site.register(VideoPublishedProxy, VideoPublishedProxyAdmin)
--- src/videos/models.py
+++ src/videos/models.py
@@ -6,12 +6,20 @@ class Video(models.Model):
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True) # 'this-is-my-video'
     video_id = models.CharField(max_length=220)
+    active = models.BooleanField(default=True)
     # timestamp
     # updated 
     # state 
     # publish_timestamp
 
-class VideoProxy(Video):
+class VideoAllProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'All Video'
+        verbose_name_plural = 'All Videos'
+
+
+class VideoPublishedProxy(Video):
     class Meta:
         proxy = True
         verbose_name = 'Published Video'
