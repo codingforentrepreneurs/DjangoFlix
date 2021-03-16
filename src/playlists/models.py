6390c7b02d9d38da59d47f191ec7898c8086338a
CFE <hello@teamcfe.com>
2021-03-16 16:58:41 -0700

    24 - Ordering ManyToMany with Through Model

--- src/playlists/models.py
+++ src/playlists/models.py
@@ -29,7 +29,7 @@ class Playlist(models.Model):
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
     video = models.ForeignKey(Video, related_name='playlist_featured', blank=True, null=True, on_delete=models.SET_NULL) # one video per playlist
-    videos = models.ManyToManyField(Video, related_name='playlist_item', blank=True)
+    videos = models.ManyToManyField(Video, related_name='playlist_item', blank=True, through='PlaylistItem')
     active = models.BooleanField(default=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -45,3 +45,12 @@ class Playlist(models.Model):
 
 pre_save.connect(publish_state_pre_save, sender=Playlist)
 pre_save.connect(slugify_pre_save, sender=Playlist)
+
+class PlaylistItem(models.Model):
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    order = models.IntegerField(default=1)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['order', '-timestamp']
