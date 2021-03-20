ae3589181ee5895292f37a9ba339c5131496d770
CFE <hello@teamcfe.com>
2021-03-20 12:45:13 -0700

    35 - User Ratings Model

---
+++ src/ratings/models.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.db import models
+
+
+User = settings.AUTH_USER_MODEL # "auth.User"
+
+class RatingChoices(models.IntegerChoices):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    __empty__ = 'Unknown'
+
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    value = models.IntegerField(null=True, blank=True, choices=RatingChoices.choices)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
