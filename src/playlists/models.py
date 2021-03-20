f4c544826a072c04b9a93e9929e327cfd130a360
CFE <hello@teamcfe.com>
2021-03-20 15:25:06 -0700

    37 - QuerySet Aggregation for Average Rating

--- src/playlists/models.py
+++ src/playlists/models.py
@@ -1,5 +1,6 @@
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
+from django.db.models import Avg, Max, Min
 from django.db.models.signals import pre_save
 from django.utils import timezone
 from django.utils.text import slugify
@@ -8,6 +9,7 @@ from djangoflix.db.models import PublishStateOptions
 from djangoflix.db.receivers import publish_state_pre_save, slugify_pre_save
 
 from categories.models import Category
+from ratings.models import Rating
 from tags.models import TaggedItem
 from videos.models import Video
 
@@ -48,12 +50,17 @@ class Playlist(models.Model):
     state = models.CharField(max_length=2, choices=PublishStateOptions.choices, default=PublishStateOptions.DRAFT)
     publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True, null=True)
     tags = GenericRelation(TaggedItem, related_query_name='playlist')
-
+    ratings = GenericRelation(Rating, related_query_name='playlist')
     objects = PlaylistManager()
     
     def __str__(self):
         return self.title
 
+    def get_rating_avg(self):
+        return Playlist.objects.filter(id=self.id).aggregate(Avg("ratings__value"))
+
+    def get_rating_spread(self):
+        return Playlist.objects.filter(id=self.id).aggregate(max=Max("ratings__value"), min=Min("ratings__value"))
 
     @property
     def is_published(self):
--- src/ratings/tests.py
+++ src/ratings/tests.py
@@ -1,6 +1,8 @@
 import random
 from django.contrib.auth import get_user_model
+from django.db.models import Avg
 from django.test import TestCase
+
 
 from playlists.models import Playlist
 from .models import Rating, RatingChoices
@@ -26,15 +28,19 @@ class RatingTestCase(TestCase):
 
     def create_ratings(self):
         items = []
+        self.rating_totals = []
         self.rating_count = 1_000
         for i in range(0, self.rating_count):
             user_obj = self.users.order_by("?").first()
             ply_obj = self.playlists.order_by("?").first()
+            rating_val = random.choice(RatingChoices.choices)[0]
+            if rating_val is not None:
+                self.rating_totals.append(rating_val)
             items.append(
                 Rating(
                     user=user_obj,
                     content_object=ply_obj,
-                    value=random.choice(RatingChoices.choices)[0]
+                    value=rating_val
                 )
             )
         Rating.objects.bulk_create(items)
@@ -66,3 +72,17 @@ class RatingTestCase(TestCase):
     def test_rating_random_choices(self):
         value_set = set(Rating.objects.values_list('value', flat=True))
         self.assertTrue(len(value_set) > 1)
+
+    def test_rating_agg(self):
+        db_avg = Rating.objects.aggregate(average=Avg('value'))['average']
+        self.assertIsNotNone(db_avg)
+        self.assertTrue(db_avg > 0)
+        total_sum = sum(self.rating_totals)
+        passed_avg = total_sum / (len(self.rating_totals) * 1.0)
+        # print(db_avg, passed_avg)
+        self.assertEqual(passed_avg, db_avg)
+
+    def test_rating_playlist_agg(self):
+        item_1 = Playlist.objects.aggregate(average=Avg('ratings__value'))['average']
+        self.assertIsNotNone(item_1)
+        self.assertTrue(item_1 > 0)
