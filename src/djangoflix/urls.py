5d6001c992bc3d83cbcd9ceb3db7071d62f26582
CFE <hello@teamcfe.com>
2021-03-22 13:46:11 -0700

    42 - URL Routing

--- src/djangoflix/urls.py
+++ src/djangoflix/urls.py
@@ -15,12 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 from playlists.views import MovieListView, TVShowListView, FeaturedPlaylistListView
+
+'''
+str - everything but /
+int - 0 and up
+slug -> this-is-a-slug-1
+uuid - import uuid; uuid.uuid4()
+path -> abc/bac/asdfads/
+
+<int:myvar>
+'''
+
 
 urlpatterns = [
     path('', FeaturedPlaylistListView.as_view()),
     path('admin/', admin.site.urls),
+    path('movies/<slug:slug>/', MovieListView.as_view()),
     path('movies/', MovieListView.as_view()),
+    path('media/<int:id>/', FeaturedPlaylistListView.as_view()),
+    path('shows/<slug:showSlug>/seasons/<slug:seasonSlug>/', TVShowListView.as_view()),
+    path('shows/<slug:slug>/seasons/', TVShowListView.as_view()),
+    path('shows/<slug:slug>/', TVShowListView.as_view()),
     path('shows/', TVShowListView.as_view()),
 ]
