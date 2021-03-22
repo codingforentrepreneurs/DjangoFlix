5e3ab2a98e75871b29bb781c78041400b2311c2e
CFE <hello@teamcfe.com>
2021-03-22 14:12:36 -0700

    43 - Detail Views

--- src/djangoflix/urls.py
+++ src/djangoflix/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from playlists.views import MovieListView, TVShowListView, FeaturedPlaylistListView
+from playlists.views import (
+    MovieListView, 
+    MovieDetailView,
+    PlaylistDetailView,
+    TVShowListView, 
+    TVShowDetailView,
+    TVShowSeasonDetailView,
+    FeaturedPlaylistListView
+)
 
 '''
 str - everything but /
@@ -31,11 +39,11 @@ path -> abc/bac/asdfads/
 urlpatterns = [
     path('', FeaturedPlaylistListView.as_view()),
     path('admin/', admin.site.urls),
-    path('movies/<slug:slug>/', MovieListView.as_view()),
+    path('movies/<slug:slug>/', MovieDetailView.as_view()),
     path('movies/', MovieListView.as_view()),
-    path('media/<int:id>/', FeaturedPlaylistListView.as_view()),
-    path('shows/<slug:showSlug>/seasons/<slug:seasonSlug>/', TVShowListView.as_view()),
-    path('shows/<slug:slug>/seasons/', TVShowListView.as_view()),
-    path('shows/<slug:slug>/', TVShowListView.as_view()),
+    path('media/<int:pk>/', PlaylistDetailView.as_view()),
+    path('shows/<slug:showSlug>/seasons/<slug:seasonSlug>/', TVShowSeasonDetailView.as_view()),
+    path('shows/<slug:slug>/seasons/', TVShowDetailView.as_view()),
+    path('shows/<slug:slug>/', TVShowDetailView.as_view()),
     path('shows/', TVShowListView.as_view()),
 ]
--- src/playlists/views.py
+++ src/playlists/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import ListView
+from django.http import Http404
+from django.views.generic import ListView, DetailView
 
-from .models import Playlist, MovieProxy, TVShowProxy
+from .models import Playlist, MovieProxy, TVShowProxy, TVShowSeasonProxy
 
 
 class PlaylistMixin():
@@ -10,6 +11,8 @@ class PlaylistMixin():
         context = super().get_context_data( *args, **kwargs)
         if self.title is not None:
             context['title'] = self.title
+
+        print(context)
         return context
 
     def get_queryset(self):
@@ -20,9 +23,35 @@ class MovieListView(PlaylistMixin, ListView):
     queryset = MovieProxy.objects.all()
     title = "Movies"
 
+class MovieDetailView(PlaylistMixin, DetailView):
+    template_name = 'playlists/movie_detail.html'
+    queryset = MovieProxy.objects.all()
+
+class PlaylistDetailView(PlaylistMixin, DetailView):
+    template_name = 'playlists/playlist_detail.html'
+    queryset = Playlist.objects.all()
+
 class TVShowListView(PlaylistMixin, ListView):
     queryset = TVShowProxy.objects.all()
     title = "TV Shows"
+
+class TVShowDetailView(PlaylistMixin, DetailView):
+    template_name = 'playlists/tvshow_detail.html'
+    queryset = TVShowProxy.objects.all()
+
+
+class TVShowSeasonDetailView(PlaylistMixin, DetailView):
+    template_name = 'playlists/season_detail.html'
+    queryset = TVShowSeasonProxy.objects.all()
+
+    def get_object(self):
+        kwargs = self.kwargs
+        show_slug = kwargs.get("showSlug")
+        season_slug = kwargs.get("seasonSlug")
+        qs = self.get_queryset().filter(parent__slug__iexact=show_slug, slug__iexact=season_slug)
+        if not qs.count() == 1:
+            raise Http404
+        return qs.first()
 
 class FeaturedPlaylistListView(PlaylistMixin, ListView):
     queryset = Playlist.objects.featured_playlists()
