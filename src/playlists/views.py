967aebc1dddeea389892c9b3d57033491ff52d66
CFE <hello@teamcfe.com>
2021-03-22 14:26:29 -0700

    44 - Get Object Exception Handling

--- src/playlists/views.py
+++ src/playlists/views.py
@@ -1,5 +1,9 @@
 from django.http import Http404
 from django.views.generic import ListView, DetailView
+from django.utils import timezone
+
+from djangoflix.db.models import PublishStateOptions
+
 
 from .models import Playlist, MovieProxy, TVShowProxy, TVShowSeasonProxy
 
@@ -48,10 +52,30 @@ class TVShowSeasonDetailView(PlaylistMixin, DetailView):
         kwargs = self.kwargs
         show_slug = kwargs.get("showSlug")
         season_slug = kwargs.get("seasonSlug")
-        qs = self.get_queryset().filter(parent__slug__iexact=show_slug, slug__iexact=season_slug)
-        if not qs.count() == 1:
+        now = timezone.now()
+        try:
+            obj = TVShowSeasonProxy.objects.get(
+                state=PublishStateOptions.PUBLISH,
+                publish_timestamp__lte=now,
+                parent__slug__iexact=show_slug,
+                slug__iexact=season_slug
+            )
+        except TVShowSeasonProxy.MultipleObjectsReturned:
+            qs = TVShowSeasonProxy.objects.filter(
+                parent__slug__iexact=show_slug,
+                slug__iexact=season_slug
+            ).published()
+            obj = qs.first()
+            # log this
+        except:
             raise Http404
-        return qs.first()
+        return obj
+
+
+        # qs = self.get_queryset().filter(parent__slug__iexact=show_slug, slug__iexact=season_slug)
+        # if not qs.count() == 1:
+        #     raise Http404
+        # return qs.first()
 
 class FeaturedPlaylistListView(PlaylistMixin, ListView):
     queryset = Playlist.objects.featured_playlists()
