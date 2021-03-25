601f358845b8be343b0f3e055d435fcaad3eb654
CFE <hello@teamcfe.com>
2021-03-25 13:19:26 -0700

    54 - Inclusion Template Tag for Ratings

--- src/ratings/models.py
+++ src/ratings/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
-
+from django.db.models import Avg
 
 User = settings.AUTH_USER_MODEL # "auth.User"
 
@@ -14,6 +14,14 @@ class RatingChoices(models.IntegerChoices):
     FIVE = 5
     __empty__ = 'Unknown'
 
+class RatingQuerySet(models.QuerySet):
+    def rating(self):
+        return self.aggregate(average=Avg("value"))['average']
+
+
+class RatingManager(models.Manager):
+    def get_queryset(self):
+        return RatingQuerySet(self.model, using=self._db)
 
 
 class Rating(models.Model):
@@ -22,3 +30,5 @@ class Rating(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
+
+    objects = RatingManager()
---
+++ src/ratings/templatetags/rating.py
@@ -0,0 +1,32 @@
+from django import template
+from django.contrib.contenttypes.models import ContentType
+
+from ratings.models import Rating
+
+register = template.Library()
+
+@register.inclusion_tag('ratings/rating.html', takes_context=True)
+def rating(context, *args, **kwargs):
+    '''
+    {% load rating %}
+    {% rating %}
+    '''
+    obj = kwargs.get("object")
+    rating_only = kwargs.get("rating_only")
+    request = context['request']
+    user = None
+    if request.user.is_authenticated:
+        user = request.user
+    
+    app_label = obj._meta.app_label # playlists
+    model_name = obj._meta.model_name # Playlist
+    c_type = ContentType.objects.get(app_label=app_label,model=model_name)
+    avg_rating = Rating.objects.filter(content_type=c_type, object_id=obj.id).rating()
+    display_form = False
+    if user is not None:
+        display_form = True
+    if rating_only is True:
+        display_form = False
+    return {
+        "value": avg_rating
+    }
